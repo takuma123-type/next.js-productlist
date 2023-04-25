9fb6c7a7034fef7a3aa5259e7d0744f4d8b89d4d
takuma123-type <seishirou1270@gmail.com>
2023-04-25 16:42:01 +0900

    componentsの切り分け

---
+++ src/components/Product_Iphone.jsx
@@ -0,0 +1,35 @@
+const PRODUCTS_IPHONE = [
+  {
+    // image: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "iPhone13Pro",
+    description: "122,800円",
+  },
+  {
+    // image: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "Iphone13",
+    description: "86,800円~",
+  },
+  {
+    // image: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "Ihone SE",
+    description: "57,800円~",
+  }
+]
+
+export function Product_Iphone() {
+  return (
+    <>
+    {PRODUCTS_IPHONE.map((product) => {
+      return (
+        <div  key={product.title}>
+          <div class="px-4 py-4 shadow-lg shadow-gray-300 rounded-2x">
+            {/* <img src={product.image} alt="" /> */}
+            <p class="mt-3">{product.title}</p>
+            <p class="mt-3">{product.description}</p>
+          </div>
+        </div>
+      );
+      })}
+    </>
+    );
+}
---
+++ src/components/Product_Mac.jsx
@@ -0,0 +1,35 @@
+const  PRODUCTS_MAC = [
+  {
+    // href: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "MacBook Air",
+    description: "115,280円~",
+  },
+  {
+    // image: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "MacBook Pro 13inch",
+    description: "148.280円~",
+  },
+  {
+    // image: "https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665",
+    title: "MacBook Pro 14inch",
+    description: "239.800円~",
+  }
+]
+
+export function Product_Mac() {
+  return (
+    <>
+    {PRODUCTS_MAC.map((product) => {
+      return (
+        <div  key={product.title}>
+          <div class="px-4 py-4 shadow-lg shadow-gray-300 rounded-2x">
+            {/* <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" />; */}
+            <p class="mt-3">{product.title}</p>
+            <p class="mt-3">{product.description}</p>
+          </div>
+        </div>
+      );
+      })}
+    </>
+    );
+}
